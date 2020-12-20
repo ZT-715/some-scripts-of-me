dadbf81339c5f5670f1dcc5fdbc0b8216d099ad2
ZT-715 <gabes.zanella@gmail.com>
2020-12-19 22:59:15 -0300

    add (LFS config): some ifs to check things

---
+++ LFSscripts/LFSconfig.sh
@@ -0,0 +1,22 @@
+#!bin/bash
+# Script to set enviromental variables to the LFS.
+
+echo 'LFS device partiton path':;
+read;
+if [[ -e ${REPLY} ]] 
+	then partition=${REPLY}
+else echo "Please insert a unmounted partition";
+fi 
+
+export LFS=/mnt/lfs;
+
+if [[ !(-e $LFS) ]] 
+	then mkdir -pv $LFS;
+fi
+mount -v -t ext4 $partition $LFS;
+
+if [[ !(-e $LFS/usr) ]] 
+	then mkdir -v ${LFS}/usr;
+fi
+mount -v -t ext4 $partition ${LFS}/usr;
+
