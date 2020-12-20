8aeb2bea8fa3a76eb715dfa5f7b1241530e941db
ZT-715 <gabes.zanella@gmail.com>
2020-12-19 23:11:58 -0300

    fix (env. config. script): semicolons

--- LFSscripts/LFSconfig.sh
+++ LFSscripts/LFSconfig.sh
@@ -1,22 +1,22 @@
 #!bin/bash
 # Script to set enviroment to the LFS.
 
-echo 'LFS device partiton path':;
-read;
+echo 'LFS device partiton path':
+read
 if [[ -e ${REPLY} ]] 
 	then partition=${REPLY}
-else echo "Please insert a unmounted partition";
-fi 
+else echo "Please insert a unmounted partition"
+fi
 
 export LFS=/mnt/lfs;
 
 if [[ !(-e $LFS) ]] 
-	then mkdir -pv $LFS;
+	then mkdir -pv $LFS
 fi
-mount -v -t ext4 $partition $LFS;
+mount -v -t ext4 $partition $LFS
 
 if [[ !(-e $LFS/usr) ]] 
-	then mkdir -v ${LFS}/usr;
+	then mkdir -v ${LFS}/usr
 fi
-mount -v -t ext4 $partition ${LFS}/usr;
+mount -v -t ext4 $partition ${LFS}/usr
 
