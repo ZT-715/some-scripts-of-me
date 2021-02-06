272bb814bd975ad44371a8865e6950d1bdd4b3f3
ZT-715 <gabes.zanella@gmail.com>
2021-02-06 16:16:32 -0300

    fix: conditions order

--- LFSscripts/LFSmount.sh
+++ LFSscripts/LFSmount.sh
@@ -3,14 +3,17 @@
 
 echo 'LFS device partiton path:'
 read partition
+
 if [[ -e $partition ]] 
 	then echo '...'
+
+	export LFS=/mnt/lfs;
+
+	if [[ !(-e $LFS) ]] 
+		then mkdir -pv $LFS
+	fi
+	mount -v -t ext4 $partition $LFS
+
 else echo "Please insert an usable unmounted partition"
 fi
 
-export LFS=/mnt/lfs;
-
-if [[ !(-e $LFS) ]] 
-	then mkdir -pv $LFS
-fi
-mount -v -t ext4 $partition $LFS
